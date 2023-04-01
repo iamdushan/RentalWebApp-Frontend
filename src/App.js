c3e36b06c227fba8fe9931294b8fb3276c8bfbb4
iamdushan <iamdushan95@gmail.com>
2023-04-01 22:46:53 +0530

    Developed movieForm, NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import "./App.css";
-import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
 import React, { Component } from "react";
+import Movies from "./components/movies";
+import Rentals from "./components/rentals";
+import NavBar from "./components/navBar";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 class App extends Component {
-  state = {};
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies"></Redirect>
+            <Redirect to="/not-found"></Redirect>
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
